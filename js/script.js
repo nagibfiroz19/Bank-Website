99333ba1d936f6e88bbf847191e16bc40cfe928a
nagibfiroz19 <nagibfiroz.seu19@gmail.com>
2021-08-15 03:46:39 +0600

    add

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 document.getElementById('login-page').addEventListener('click',function(){
     const emailField=document.getElementById('user-email');
     const userEmail=emailField.value;
-    console.log(userEmail);
     const passwordField=document.getElementById('user-password');
     const userPassword=passwordField.value;
- console.log(userPassword);
   
+   if(userEmail== 'nagib@gmail.com' && userPassword=='nagib'){
+           
+   }
+   else{
+       console.log('Email & Password mismatch');
+   }
 })
